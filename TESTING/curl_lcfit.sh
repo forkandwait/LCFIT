7f291f0a4620a2a6b34f3dc24dd896bdc27dd93a
webbs <webbs@demog.berkeley.edu>
2010-09-26 17:29:57 -0700

    New test file with curl, to be run at home, etc

---
+++ TESTING/curl_lcfit.sh
@@ -0,0 +1,15 @@
+set -e 
+
+ID="$$$RANDOM"
+COOKIEF="lcfit_cookies.$ID"
+LCFITURL="http://lcfit.demog.berkeley.edu/lcfit/lc"
+OUTF="lcfit_output.$ID"
+
+echo "Logging in" >> $OUTF
+curl --silent -S --fail -b "$COOKIEF" -c "$COOKIEF" -d "USERNAME=webbs&PASSWORD=foobar" \
+	"$LCFITURL/LoginProcess" 1>>$OUTF
+
+echo "Getting index" >> $OUTF
+curl --silent -S --fail -b "$COOKIEF" -c "$COOKIEF"  \
+	"$LCFITURL/index" 1>>$OUTF
+
